204c152247ad9fd40861d14b081ea98867f80ef3
James Halliday <mail@substack.net>
2015-05-12 01:16:56 -0700

    root

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ var split = require('split2');
 var through = require('through2');
 
 var spawn = require('child_process').spawn;
-var ps = spawn('ssh', [ 'omnidoor.local', 'pm2 logs --raw doorjam' ]);
+var ps = spawn('ssh', [ 'root@omnidoor.local', 'pm2 logs --raw doorjam' ]);
 ps.on('exit', process.exit);
 ps.stderr.pipe(process.stderr);
 ps.stdout.pipe(process.stdout);
