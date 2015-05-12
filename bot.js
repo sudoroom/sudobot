bb178e4d81362ef9cc933952442675f90d2e313b
James Halliday <mail@substack.net>
2015-05-12 11:34:00 -0700

    more events

--- bot.js
+++ bot.js
@@ -10,14 +10,22 @@ var through = require('through2');
 
 var spawn = require('child_process').spawn;
 var ps = spawn('ssh', [ 'root@omnidoor.local', 'pm2 logs --raw doorjam' ]);
+var lastmsg = 0;
+
 ps.on('exit', process.exit);
 ps.stderr.pipe(process.stderr);
 ps.stdout.pipe(process.stdout);
 ps.stdout.pipe(split())
     .pipe(through(function (buf, enc, next) {
         var line = buf.toString();
-        if (/^Access granted/.test(line)) {
-            client.say('#sudoroom', 'somebody swiped into the building');
+        if (/^Access granted/i.test(line)) {
+            client.say('#sudoroom', 'DOOR EVENT: somebody swiped into the building');
+        }
+        else if (/^Everything initialized and ready/i.test(line)) {
+            client.say('#sudoroom', 'DOOR EVENT: READY');
+        }
+        else if (/^Error:/i.test(line)) {
+            client.say('#sudoroom', 'DOOR EVENT: ' + line);
         }
         next();
     }))
