781b8c6b3dcfc9c6fd8c11fcb53a10c991bd4119
James Halliday <mail@substack.net>
2015-05-19 18:17:27 +0200

    connection timeout before deciding the connection works

--- bot.js
+++ bot.js
@@ -10,17 +10,20 @@ var through = require('through2');
 
 var lastmsg = 0;
 var failing = {};
-var connected = false;
+var timeout = null;
+
+function say (msg) {
+    client.say(msg);
+}
+
 ssh();
 
 function ssh () {
-    if (connected) return;
-    connected = true;
-    
     var spawn = require('child_process').spawn;
     var ps = spawn('ssh', [ 'root@omnidoor.local', 'psy log doorjam' ]);
+    
     ps.on('exit', function () {
-        connected = false;
+        clearTimeout(timeout);
         setTimeout(ssh, 5000);
     });
     ps.stderr.pipe(process.stderr);
@@ -28,38 +31,33 @@ function ssh () {
     ps.stdout.pipe(split()).pipe(through(write, end));
     
     function write (buf, enc, next) {
-        if (failing.ssh) {
-            client.say('#sudoroom', 'DOOR EVENT: omnidoor ssh connection established');
+        if (failing.ssh && !timeout) {
+            timeout = setTimeout(function () {
+                say('#sudoroom', 'DOOR EVENT: omnidoor ssh connection established');
+                failing.ssh = false;
+                timeout = null;
+            }, 5000)
         }
-        failing.ssh = false;
         
         var line = buf.toString();
         if (/^Access granted/i.test(line)) {
             failing.logs = false;
-            client.say('#sudoroom', 'DOOR EVENT: somebody swiped into the building');
+            say('#sudoroom', 'DOOR EVENT: somebody swiped into the building');
         }
         else if (/^Everything initialized and ready/i.test(line)) {
             failing.logs = false;
-            client.say('#sudoroom', 'DOOR EVENT: READY');
+            say('#sudoroom', 'DOOR EVENT: READY');
         }
         else if (/^Error:/i.test(line)) {
             failing.logs = false;
-            client.say('#sudoroom', 'DOOR EVENT: ' + line);
+            say('#sudoroom', 'DOOR EVENT: ' + line);
         }
         else if (/^Can not find log files,/.test(line)) {
             if (!failing.logs) {
-                client.say('#sudoroom', 'DOOR EVENT: log read failure');
+                say('#sudoroom', 'DOOR EVENT: log read failure');
             }
             failing.logs = true;
         }
         next();
-    }
-    function end () {
-        if (!failing.ssh) {
-            client.say('#sudoroom', 'DOOR EVENT: omnidoor ssh connection closed');
-        }
-        failing.ssh = true;
-        connected = false;
-        setTimeout(ssh, 5000);
     }
 }
