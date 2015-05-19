ec6e4e775c4149331faf1ff164ecf830b477d418
James Halliday <mail@substack.net>
2015-05-19 17:48:13 +0200

    psy

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ function ssh () {
     connected = true;
     
     var spawn = require('child_process').spawn;
-    var ps = spawn('ssh', [ 'root@omnidoor.local', 'pm2 logs --raw doorjam' ]);
+    var ps = spawn('ssh', [ 'root@omnidoor.local', 'psy logs doorjam' ]);
     ps.on('exit', function () {
         connected = false;
         setTimeout(ssh, 5000);
