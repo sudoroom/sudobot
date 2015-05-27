53553dc51f236432d18ccae51f325f6b5d8cdc74
James Halliday <mail@substack.net>
2015-05-27 11:35:59 +0200

    full

--- health.js
+++ health.js
@@ -6,7 +6,7 @@ check();
 
 function check () {
     exec('acpi', function (err, stdout, stderr) {
-        var m = /^Battery \d+: (Charging|Discharging), (\d+)%/.exec(stdout);
+        var m = /^Battery \d+: (Charging|Discharging|Full), (\d+)%/.exec(stdout);
         var msg = {};
         if (m) {
             msg.charging = m[1] !== 'Discharging';
