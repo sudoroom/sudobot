ca1833718efeeb5bb1974bc7f2987814713af6e7
James Halliday <mail@substack.net>
2015-05-27 10:00:36 +0200

    health checker

---
+++ health.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+var exec = require('child_process').exec;
+
+setInterval(check, 1000*60);
+check();
+
+function check () {
+    exec('acpi', function (err, stdout, stderr) {
+        var m = /^Battery \d+: (Charging|Discharging), (\d+)%/.exec(stdout);
+        var msg = {};
+        if (m) {
+            msg.charging = m[1] === 'Charging';
+            msg.percent = Number(m[2]);
+        }
+        console.log(msg);
+    });
+}
